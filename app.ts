8c9e33a94bd0147e3452d4407f55dc29287d90d0
Pavel <todizzy@gmail.com>
2019-08-15 16:24:40 +0300

    Both express and fastify adapters test

--- app.ts
+++ app.ts
@@ -1,10 +1,33 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, AbstractHttpAdapter } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import fetch from 'node-fetch';
 
 run();
 
 async function run() {
-  const app = await NestFactory.create(AppModule);
+  await test(new ExpressAdapter());
+  await test(new FastifyAdapter());
+}
+
+async function test(adapter: AbstractHttpAdapter) {
+  const app = await NestFactory.create(AppModule, adapter);
   await app.listen(3000);
-  console.log('ok');
+
+  console.log(adapter.constructor.name);
+  await call('/graphql');
+  await call('/ee');
+
+  await app.close();
+}
+
+async function call(endpoint: string) {
+  const { status, statusText } = await fetch(
+    'http://localhost:3000' + endpoint,
+    {
+      headers: { Accept: 'text/html' }
+    }
+  );
+  console.log(endpoint, status, statusText);
 }
